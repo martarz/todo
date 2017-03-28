82cbd568413db91d4526ef4c22af27ebafdb0428
Marta Rzadkowska <martarzadkowska@gmail.com>
2017-03-28 17:52:32 +0200

    css in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid, ButtonToolbar, ButtonGroup, Button } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 import TodoInput from './input'
 
 class App extends Component {
   render() {
     return (
-      <Grid>
+      <Grid className="App">
         <div>
           <h2>React ToDos App</h2>
         </div>
-
-
         <TodoInput/>
 
 
-
-
-        <ButtonToolbar>
-          <ButtonGroup>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>4</Button>
-          </ButtonGroup>
-        </ButtonToolbar>
       </Grid>
     );
   }
